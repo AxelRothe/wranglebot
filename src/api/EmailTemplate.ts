f7bcb7638f7c0bc70d81906c5d9738f6c74aba95
Axel Rothe <a.rothe@vanrothe.com>
2023-05-07 13:51:30 +0200

    - corrects background color of emails

--- src/api/EmailTemplate.ts
+++ src/api/EmailTemplate.ts
@@ -133,7 +133,7 @@ class EmailTemplate {
         }
       }
       </style></head>
-        <body class style="background-color: #eaebed; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+        <body class style="background-color: #151d21; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
           <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; min-width: 100%; background-color: #eaebed; width: 100%;" width="100%" bgcolor="#eaebed">
             <tr>
               <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
